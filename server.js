583addec183c2eeb6f21e774276566b599e93c4f
James Garcia <cooljj_20@sbcglobal.net>
2019-04-01 21:24:16 -0500

    Working Create and Join room calls

--- server.js
+++ server.js
@@ -5,10 +5,33 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-
-io.on('connection', function(socket){
-  console.log('a user connected');
+var newRoom = "1";
+var rooms = io.sockets.adapter.rooms;
+// handle incoming connections from clients
+io.sockets.on('connection', function(socket) {
+  socket.on('create-room', function() {
+    createRoom(socket);
+  });
+  socket.on('join-room', function(room) {
+    joinRoom(socket, room);
+  });
 });
+
+function createRoom(socket) {
+  socket.join(newRoom);
+  console.log(newRoom);
+  var roomInt = parseInt(newRoom);
+  newRoom = `${roomInt}`;
+  roomInt += 1;
+  console.log("Created a Room");
+};
+
+function joinRoom(socket, room) {
+  socket.join(room);
+  console.log("Joined a Room");
+};
+
+
 
 var port = process.env.PORT || 3000;
 
